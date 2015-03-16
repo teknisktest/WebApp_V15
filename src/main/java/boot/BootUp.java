5bb1c30a309d1e8e9d2d4d104c08689e92c40ced
olovholm <ola.lovholm@steria.no>
2015-03-16 21:09:16 +0100

    Legger til ComponentScan i BootUp-klassen. Dette gjør at vi sjekker andre filer for å se om disse brukes (gjerne ved at de har en eller annen @nnotasjon)

--- src/main/java/boot/BootUp.java
+++ src/main/java/boot/BootUp.java
@@ -2,21 +2,15 @@ package boot;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 @EnableAutoConfiguration
+@ComponentScan
 public class BootUp {
-	
-	
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World";
-	}
-	
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(BootUp.class, args);
@@ -29,8 +23,7 @@ public class BootUp {
 			return "YEAH YEAH";
 		}
 	}
-	
-	
+		
 
 }
 
