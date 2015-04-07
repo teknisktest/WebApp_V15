4a0578dbe39ef7e57e458ec26c30de92ec2b8b5c
olovholm <ola.lovholm@steria.no>
2015-04-07 12:18:22 +0200

    Changes PersonController

--- src/main/java/boot/PersonController.java
+++ src/main/java/boot/PersonController.java
@@ -3,6 +3,8 @@ package boot;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,11 +12,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PersonController {
 	
-	@RequestMapping(value = "/person")
-	public String getPerson(Model model){
+	@RequestMapping(value = "/person", method=RequestMethod.GET)
+	public String getPersonInfoFromDefault(Model model){
 		model.addAttribute("name", "Ditt navn");
 		model.addAttribute("age", "Din alder");
 		model.addAttribute("food", "Din favorittmat");
+		
+		return "page";
+	}
+	
+	@RequestMapping(value = "/person", method=RequestMethod.POST)
+	public String getPersonInfoFromPostMethod(@RequestParam("myName") String myName, 
+			@RequestParam("myFood") String myFood, 
+			Model model){
+		model.addAttribute("name", myName);
+		model.addAttribute("age", "Din alder");
+		model.addAttribute("food", myFood);
 		
 		return "page";
 	}
